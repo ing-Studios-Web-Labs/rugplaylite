f2a53e900b92ab4b42b77e5fcbd07bb7a16239d2
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2025-09-19 21:41:27 +0800

    Delete RugplayLite v1.0.0 directory

--- RugplayLite v1.0.0/public/global/homepageRedirect.js
+++
@@ -1,6 +0,0 @@
-export function redirectToHomepage(homepageIconID) {
-    const homepageIcon = document.getElementById(homepageIconID);
-    homepageIcon.addEventListener('click', () => {
-        document.location.href = '/homepage/homepage.html';
-    });
-}
